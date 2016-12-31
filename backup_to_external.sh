818c007caa3ad748bf3e148592bcb093bf5354c3
Andreas Zweili <andreas@2li.ch>
2016-12-31 15:47:41 +0100

    fix path

--- backup_to_external.sh
+++ backup_to_external.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SCRIPTPATH=$(dirname -- "$(readlink -e -- "$BASH_SOURCE")")
 PATH=$PATH:$SCRIPTPATH/bin/
-source $SCRIPTPATH/bin/activate
-python3 $SCRIPTPATH/backup_to_external.py
+source $SCRIPTPATH/borg_interface/bin/activate
+python3 $SCRIPTPATH/borg_interface/backup_to_external.py
 exit 0
