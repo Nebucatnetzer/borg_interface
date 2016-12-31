08cee7696c2d350583e5a3941aa9dc633de011f5
Andreas Zweili <andreas@2li.ch>
2016-12-31 14:31:40 +0100

    add environment variables to the bash script

--- backup_to_external.sh
+++ backup_to_external.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-PATH=$PATH:borg_interface/bin/
-source borg_interface/bin/activate
-python3 borg_interface/backup_to_external.py
+INTERFACE_PATH=/home/andreas/.virtualenvs/borg_interface
+PATH=$PATH:$INTERFACE_PATH/bin/
+source $INTERFACE_PATH/bin/activate
+python3 $INTERFACE_PATH/backup_to_external.py
 exit 0
